c0067e54aa323bb5bcdce93c80dab6d5814bd0ad
aleks <aleksandar.stojanovic@bloxico.com>
2021-02-05 21:43:57 +0100

    added Mantis config.json clean up after test

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,6 +4,8 @@ const APP_CONF = {
         LINUX_LOCAL: "/home/aleks/Desktop/IOHK/Mantis-Wallet/Mantis-Wallet-0.1.1.AppImage"
     },
     START_TIMEOUT:30000,
-    WAIT:30000
+    WAIT:30000,
+    CONF_PATH: "C:\\Users\\aleks\\.mantis-wallet\\config.json",
+    BASE_CONF:"C:\\Users\\aleks\\OneDrive\\Desktop\\IOHK\\iohk-mantis\\test_data\\config.json"
 };
 module.exports = APP_CONF
